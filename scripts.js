7710baee09d99af8fd904ee6098b7d88f7704ada
Curtis-Lee Waterboer <waterboercurtislee@gmail.com>
2024-06-25 21:54:45 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,26 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const openPopupBtn = document.getElementById('openPopupBtn');
     const closePopupBtn = document.getElementById('closePopupBtn');
     const popup = document.getElementById('popup');
-    const signUpForm = document.getElementById('signUpForm');
-    const loginForm = document.getElementById('loginForm');
-    const switchToLogin = document.getElementById('switchToLogin');
-    const switchToSignUp = document.getElementById('switchToSignUp');
 
     openPopupBtn.addEventListener('click', function () {
-        popup.classList.remove('hidden');
+        popup.classList.toggle('active');
+        openPopupBtn.classList.add('hidden');
     });
 
     closePopupBtn.addEventListener('click', function () {
-        popup.classList.add('hidden');
-    });
-
-    switchToLogin.addEventListener('click', function () {
-        signUpForm.classList.remove('active');
-        loginForm.classList.add('active');
-    });
-
-    switchToSignUp.addEventListener('click', function () {
-        loginForm.classList.remove('active');
-        signUpForm.classList.add('active');
+        popup.classList.remove('active');
+        openPopupBtn.classList.remove('hidden');
     });
 });
